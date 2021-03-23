8a985ef151eb4f2ebe3f9505deec8df94bdb09ad
iayyyyy_sei <2190494@ecccomp.com>
2021-03-23 13:25:08 +0900

    image added Search.php changed

--- classes/Search.php
+++ classes/Search.php
@@ -14,20 +14,20 @@ class Search extends Databases
             $_SESSION['search_id'] = $row['rest_id'];
             $rest_name = $row['rest_name'];
             $rest_address = $row['rest_address'];
-            $rest_image = base64_encode($row['rest_image']);
+            $rest_image = ($row['rest_image']);
 
+            
+            echo '<div class="row portfolio-container" data-aos="fade-up">';
 
-            echo '<div class="row portfolio-container" data-aos="fade-up">
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                        <img src=data:base64,' . "$rest_image" . 'class="img-fluid" alt="">
-                        <div class="portfolio-info">
-                            <h4>' . $rest_name . '</h4>
-                            <p>' . $rest_address. '</p>
-                            <br>
+                    echo '<div class="col-lg-4 col-md-6 portfolio-item filter-app">';
+                        echo "<img src=$rest_image class='img-fluid' alt=''>";
+                        echo '<div class="portfolio-info">';
+                            echo "<h4>$rest_name</h4>";
+                            echo '<p>' . $rest_address. '</p>';
+                            echo "
                         </div>
                     </div>
-                </div>';
+                </div>";
 
 
         }
